001081f62294c37906c3de988dc3d1b1a6c6d496
Minura Dinupriya <mranaweera793@gmail.com>
2024-04-22 15:22:32 +0530

    Set responsive padding to the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,9 @@
 const Navbar=()=>{
     return (
-        <div className="">Navbar</div>
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            <div>1</div>
+            <div>2</div>
+        </div>
     )
 }
 
