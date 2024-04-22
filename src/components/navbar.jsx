c9ab4bef807413c19ce365406d3ba3bba2fb4cbe
Minura Dinupriya <mranaweera793@gmail.com>
2024-04-22 15:42:16 +0530

    Created the logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,25 @@
-const Navbar=()=>{
-    return (
-        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-            <div>1</div>
-            <div>2</div>
-        </div>
-    )
-}
+import Homepage from "@/app/page";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* logo */}
+      <div className="">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold"
+        >
+          <span className="text-white ">Minura</span>
+        </Link>
+        <Link href="/">
+          <span className="w-12 h-8 rounded bg-white text-black">.dev</span>
+        </Link>
+      </div>
+      {/* responsive menu */}
+      <div className=""></div>
+    </div>
+  );
+};
 
 export default Navbar;
