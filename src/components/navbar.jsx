f45779d3e4c03a7ec03143412e286da3a3532cca
Minura Dinupriya <mranaweera793@gmail.com>
2024-04-25 08:09:06 +0530

    Add social links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Homepage from "@/app/page";
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const links = [
   { url: "/", title: "Home" },
@@ -9,10 +13,21 @@ const links = [
 ];
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* links */}
+      <div className="hidden md:flex gap-4 w-1/3 justify-center">
+        {links.map((link) => (
+          <Link href={link.url} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+
       {/* logo */}
-      <div className="">
+      <div className="md:hidden lg:flex w-1/3 justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold  flex items-center justify-center"
@@ -22,22 +37,51 @@ const Navbar = () => {
             .dev
           </span>
         </Link>
-        <Link href="/"></Link>
       </div>
+
+      {/* social */}
+      <div className="hidden md:flex gap-4 w-1/3 justify-center">
+        <Link href="/">
+          <Image src="/github.png" alt="asdf" width={24} height={24}></Image>
+        </Link>
+        <Link href="/">
+          <Image src="/dribbble.png" alt="asdf" width={24} height={24}></Image>
+        </Link>
+        <Link href="/">
+          <Image src="/instagram.png" alt="asdf" width={24} height={24}></Image>
+        </Link>
+        <Link href="/">
+          <Image src="/facebook.png" alt="asdf" width={24} height={24}></Image>
+        </Link>
+        <Link href="/">
+          <Image src="/pinterest.png" alt="asdf" width={24} height={24}></Image>
+        </Link>
+        <Link href="/">
+          <Image src="/linkedin.png" alt="asdf" width={24} height={24}></Image>
+        </Link>
+      </div>
+
       {/* responsive menu */}
-      {/* menu button */}
-      <button className="flex flex-col justify-between w-10 h-8">
-        <div className="w-10 h-1 bg-black rounded"></div>
-        <div className="w-10 h-1 bg-black rounded"></div>
-        <div className="w-10 h-1 bg-black rounded"></div>
-      </button>
-      {/* menu list */}
-      <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl">
-        {links.map((link, index) => (
-          <Link href={link.url} key={index}>
-            {link.title}
-          </Link>
-        ))}
+      <div className="md:hidden">
+        {/* menu button */}
+        <button
+          className="flex flex-col justify-between w-10 h-8 z-50 relative"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+        </button>
+        {/* menu list */}
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl">
+            {links.map((link, index) => (
+              <Link href={link.url} key={index}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
