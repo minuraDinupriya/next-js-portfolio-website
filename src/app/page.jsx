28e96988fa99de82526bdbaca9c461797833b1c8
Minura Dinupriya <mranaweera793@gmail.com>
2024-04-26 13:17:16 +0530

    Add image to the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col">
+      {/* image container */}
+      <div className="h-1/2 relative">
+        <Image src="/hero.png" alt="" className="object-contain" fill ></Image>
+      </div>
+      {/* text container */}
+      <div className="h-1/2"></div>
+      {/* button container */}
+    </div>
+  );
 };
 
 export default Homepage;
