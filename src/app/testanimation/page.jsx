3bffbbfa18e39c5c3d2114eb402afff0281a5a43
Minura Dinupriya <mranaweera793@gmail.com>
2024-04-28 09:52:41 +0530

    Sample animations

--- src/app/testanimation/page.jsx
+++ src/app/testanimation/page.jsx
@@ -1,13 +1,29 @@
-"use client"
-import {motion} from "framer-motion"
+"use client";
+import { delay, motion } from "framer-motion";
 
-const TestPage=()=>{
-    return(
-        <div className="h-full flex items-center justify-center ">
-            <motion.div className="w-96 h-96 bg-red-400 rounded" initial={{x:-100}} animate={{x:100}}></motion.div>
-        </div>
-        
-    )
-}
+const TestPage = () => {
+  const variants = {
+    //animation variants
+    variant1: {
+      x: 400,
+      y: 300,
+      opacity: 0.5,
+      transition: { delay: 1, duration: 4 },
+    },
+    variant2: { x: 100, y: -300, rotation: 90 },
+  };
+
+  return (
+    <div className="h-full flex items-center justify-center ">
+      <motion.div
+        className="w-96 h-96 bg-red-400 rounded"
+        initial={{ x: -100 }}
+        variants={variants}
+        animate="variant1"
+        // transition={{ delay: 1, duration: 4 }}
+      ></motion.div>
+    </div>
+  );
+};
 
 export default TestPage;
