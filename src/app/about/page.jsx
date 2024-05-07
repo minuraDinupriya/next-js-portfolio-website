33dd1e23ae3f034383196d36c5495cece841ca6f
Minura Dinupriya <mranaweera793@gmail.com>
2024-05-07 20:20:38 +0530

    Animate about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,13 @@
+import { motion } from "framer-motion";
+
 const About=()=>{
     return (
-        <div className="">About</div>
+        <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    ></motion.div>
     )
 }
 
