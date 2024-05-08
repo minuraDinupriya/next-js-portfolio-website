45597f41ea1a755872de3fb48705789cbd18f0e2
Minura Dinupriya <mranaweera793@gmail.com>
2024-05-08 20:37:55 +0530

    Add starting animation

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,13 @@
+import { motion } from "framer-motion";
+
 const Contact=()=>{
     return (
-        <div className="">Contact</div>
+        <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    ></motion.div>
     )
 }
 
