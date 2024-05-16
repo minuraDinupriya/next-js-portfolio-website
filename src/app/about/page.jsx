8667c7c442dceecea7fb60587158de89af76b966
Minura Dinupriya <mranaweera793@gmail.com>
2024-05-16 11:56:44 +0530

    Add stylings to the experiences section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,7 +10,7 @@ const About = () => {
       transition={{ duration: 1 }}
     >
       {/* About container */}
-      <div className="">
+      <div className="h-full overflow-scroll">
         {/* TEXT CONTAINER */}
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 text-xl flex flex-col gap-24 md:m-32 lg:gap-40 xl:gap-64">
           {/* BIOGRAPHY CONTAINER */}
@@ -110,98 +110,97 @@ const About = () => {
             <div>
 
               {/* EXPERIENCE LIST ITEM 1*/}
-              <div>
+              <div className="flex justify-between h-48">
                 {/* LEFT */}
-                <div>
+                <div className="w-1/3">
                   {/* JOB TITLE */}
-                  <div>Intern software developer</div>
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">Intern software developer</div>
                   {/* JOB DESCRIPTION */}
-                  <div>
+                  <div className="p-3 text-sm italic">
                     My first ever industry experience. Learning day by day.
                   </div>
                   {/* JOB DATE */}
-                  <div>
+                  <div className="p-3 text-sm text-red-400 font-semibold">
                     2024 february - present
                   </div>
                   {/* COMPANY NAME */}
-                  <div>
+                  <div className="p-1 bg-white text-sm font-semibold w-fit">
                     Techcciance
                   </div>
                 </div>
                 {/* CENTER */}
-                <div>
+                <div className="w-1/6">
                   {/* LINE */}
-                  <div>
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
                     {/* LINE CIRCLE */}
-                    <div></div>
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                   </div>
                 </div>
                 {/* RIGHT */}
-                <div></div>
+                <div className="w-1/3"></div>
               </div>
 
               {/* EXPERIENCE LIST ITEM 2*/}
-              <div>
+              <div className="flex justify-between h-48">
                 {/* LEFT */}
-                
-                </div>
+                <div className="w-1/3"></div>
                 {/* CENTER */}
-                <div>
+                <div className="w-1/6">
                   {/* LINE */}
-                  <div>
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
                     {/* LINE CIRCLE */}
-                    <div></div>
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                   </div>
                 </div>
                 {/* RIGHT */}
-                <div>
-                <div>
+                
+                <div className="w-1/3">
                   {/* JOB TITLE */}
-                  <div>Intern software developer</div>
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">Intern software developer</div>
                   {/* JOB DESCRIPTION */}
-                  <div>
+                  <div className="p-3 text-sm italic">
                     My first ever industry experience. Learning day by day.
                   </div>
                   {/* JOB DATE */}
-                  <div>
+                  <div className="p-3 text-sm text-red-400 font-semibold">
                     2024 february - present
                   </div>
                   {/* COMPANY NAME */}
-                  <div>
+                  <div className="p-1 bg-white text-sm font-semibold w-fit">
                     Techcciance
                   </div>
                 </div>
               </div>
 
               {/* EXPERIENCE LIST ITEM 3*/}
-              <div>
+              <div className="flex justify-between h-48">
                 {/* LEFT */}
-                <div>
+                <div className="w-1/3">
                   {/* JOB TITLE */}
-                  <div>Intern software developer</div>
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">Intern software developer</div>
                   {/* JOB DESCRIPTION */}
-                  <div>
+                  <div className="p-3 text-sm italic">
                     My first ever industry experience. Learning day by day.
                   </div>
                   {/* JOB DATE */}
-                  <div>
+                  <div className="p-3 text-sm text-red-400 font-semibold">
                     2024 february - present
                   </div>
                   {/* COMPANY NAME */}
-                  <div>
+                  <div className="p-1 bg-white text-sm font-semibold w-fit">
                     Techcciance
                   </div>
                 </div>
                 {/* CENTER */}
-                <div>
+                <div className="w-1/6">
                   {/* LINE */}
-                  <div>
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
                     {/* LINE CIRCLE */}
-                    <div></div>
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                   </div>
                 </div>
                 {/* RIGHT */}
-                <div></div>
+                <div className="w-1/3"></div>
               </div>
 
             </div>
