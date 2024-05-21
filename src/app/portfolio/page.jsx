80576fbba637cba642ac1318f158168f578a76b2
Minura Dinupriya <mranaweera793@gmail.com>
2024-05-21 07:46:52 +0530

    Create basic sketch

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,14 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const Portfolio = () => {
+  const ref=useRef();
+  const scrollYProgress=useScroll();
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
+
+  
+
   return (
     <motion.div
       className="h-full"
@@ -9,7 +16,16 @@ const Portfolio = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Portfolio
+      <div className="h-[600vh] relative" ref={ref}>
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+
+        </div>
+
+        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+
+
+        </div>
+      </div>
     </motion.div>
   );
 };
