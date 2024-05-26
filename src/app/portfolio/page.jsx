a04fa9cc595a1280705401e6b7237c9f18365529
Minura Dinupriya <mranaweera793@gmail.com>
2024-05-26 08:59:58 +0530

    Add view demo button to see sample projects

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -69,11 +69,14 @@ const PortfolioPage = () => {
                     {item.title}
                   </h1>
                   <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
-                    <Image src={item.img} alt="" fill />
+                    {/* <Image src={item.img} alt="" fill /> */}
                   </div>
                   <p className="w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]">
                     {item.desc}
                   </p>
+                  <Link href={item.link} className="flex justify-end">
+                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">See Demo</button>
+                  </Link>
                 </div>
               </div>
             ))}
