0d8ff14f12f803f33fc90165bfe93a2d257cda7a
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-02 18:46:59 +0530

    Create say hello animation

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,14 +1,38 @@
 "use client";
 import { motion } from "framer-motion";
 import { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
+// import emailjs from "@emailjs/browser";
 
-const Contact = () => {
+const ContactPage = () => {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
   const text = "Say Hello";
 
   const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+  //   setError(false);
+  //   setSuccess(false);
+
+  //   emailjs
+  //     .sendForm(
+  //       process.env.NEXT_PUBLIC_SERVICE_ID,
+  //       process.env.NEXT_PUBLIC_TEMPLATE_ID,
+  //       form.current,
+  //       process.env.NEXT_PUBLIC_PUBLIC_KEY
+  //     )
+  //     .then(
+  //       () => {
+  //         setSuccess(true);
+  //         form.current.reset();
+  //       },
+  //       () => {
+  //         setError(true);
+  //       }
+  //     );
+  // };
+
   return (
     <motion.div
       className="h-full"
@@ -16,9 +40,32 @@ const Contact = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Contact
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+        </div>
+        {/* FORM CONTAINER */}
+        
+      </div>
     </motion.div>
   );
 };
 
-export default Contact;
+export default ContactPage;
