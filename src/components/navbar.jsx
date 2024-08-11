8fac7a1f58153378990ab9a601b2639203e56e7f
Minura Dinupriya <mranaweera793@gmail.com>
2024-08-11 17:57:45 +0530

    Minor changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-
-import Homepage from "@/app/page";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
